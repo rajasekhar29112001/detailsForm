49854d3ae9981aaa13c5fd88761f6ec767cb9e1c
rajasekhar29112001 <krajasekhar2901@gmail.com>
2025-02-13 15:43:32 +0530

    final submit

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     const [arr, setArr] = useState([]);
     const [obj, setObj] = useState({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: 0, experience: [{ company: "", role: "", years: "", stDate: "", enDate: "" }] });
 
-    const [indexVal, setIndexVal] = useState(null);
+    const [indexVal, setIndexVal] = useState();
     const [bool, setBool] = useState(false);
 
     const [searchObj, setSearchObj] = useState({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: "" });
@@ -19,10 +19,42 @@ const NavBar = () => {
         setObj({ ...obj, experience: [...obj.experience, { company: "", role: "", years: "", stDate: "", enDate: "" }] });
     }
 
-    function deleteExperi(index) {
-        const updateExper = obj.experience.filter((item, i) => i !== index);
-        setObj({ ...obj, experience: updateExper });
-        sumOfYears();
+    function saveData() {
+        (!obj.id || !obj.name || !obj.email || !obj.age || !obj.doj || !obj.gender) ? alert("Please enter all fields") : filterDupData();
+    }
+
+    function filterDupData() {
+        if (arr.some(e => e.id === obj.id)) {
+            alert("This ID is already recorded.");
+        } else {
+            let updateData = [...arr, obj];
+            console.log("updateData = ", updateData)
+            setArr(updateData);
+            setFilArr(updateData);
+        }
+        setObj({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: 0, experience: [{ company: "", role: "", years: "", stDate: "", enDate: "" }] });
+    }
+
+    function editData(index) {
+        setObj(arr[index]);
+        setIndexVal(index);
+        setBool(true);
+        setDisplay("details");
+    }
+
+    function updateData() {
+        let updatedArr = [...arr];
+        updatedArr[indexVal] = obj;
+        setArr(updatedArr);
+        setFilArr(updatedArr);
+        setBool(false);
+        setObj({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: 0, experience: [{ company: "", role: "", years: "", stDate: "", enDate: "" }] });
+    }
+    
+    function deleteData(ind) {
+        let updateArr = arr.filter((item, i) => i !== ind);
+        setArr(updateArr);
+        setFilArr(updateArr);
     }
 
     function searching(key, e) {
@@ -30,20 +62,6 @@ const NavBar = () => {
         let updatedSearchObj = { ...searchObj, [key]: value };
         setSearchObj(updatedSearchObj);
         filteredData(updatedSearchObj);
-    }
-
-    function filteredData(data) {
-        const filterData = arr.filter(item =>
-            item.id.includes(data.id) &&
-            item.name.toLowerCase().includes(data.name.toLowerCase()) &&
-            item.email.toLowerCase().includes(data.email.toLowerCase()) &&
-            item.age.includes(data.age) &&
-            item.doj.includes(data.doj) &&
-            item.gender.toLowerCase().includes(data.gender.toLowerCase()) &&
-            item.totalExperience.toString().includes(data.totalExperience)
-        );
-        console.log("filterData = ", filterData)
-        setFilArr(filterData);
     }
 
     function experienceObj(index, field, value) {
@@ -67,56 +85,37 @@ const NavBar = () => {
         setObj(obj => ({ ...obj, totalExperience: sum }));
     }
 
-    function saveData() {
-        (!obj.id || !obj.name || !obj.email || !obj.age || !obj.doj || !obj.gender) ? alert("Please enter all fields") : filterDupData();
+    function filteredData(data) {
+        const filterData = arr.filter(item =>
+            item.id.includes(data.id) &&
+            item.name.toLowerCase().includes(data.name.toLowerCase()) &&
+            item.email.toLowerCase().includes(data.email.toLowerCase()) &&
+            item.age.includes(data.age) &&
+            item.doj.includes(data.doj) &&
+            item.gender.toLowerCase().includes(data.gender.toLowerCase()) &&
+            item.totalExperience.toString().includes(data.totalExperience)
+        );
+        console.log("filterData = ", filterData)
+        setFilArr(filterData);
     }
 
-    function filterDupData() {
-        if (arr.some(e => e.id === obj.id)) {
-            alert("This ID is already recorded.");
-        } else {
-            let updateData = [...arr, obj];
-            console.log("updateData = ", updateData)
-            setArr(updateData);
-            setFilArr(updateData);
-        }
-        setObj({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: 0, experience: [{ company: "", role: "", years: "", stDate: "", enDate: "" }] });
+    function deleteExperi(index) {
+        const updateExper = obj.experience.filter((item, i) => i !== index);
+        setObj({ ...obj, experience: updateExper });
+        sumOfYears();
     }
-
-    function deleteData(ind) {
-        let updateArr = arr.filter((item, i) => i !== ind);
-        setArr(updateArr);
-        setFilArr(updateArr);
-    }
-
-    function editData(index) {
-        setObj(arr[index]);
-        setIndexVal(index);
-        setBool(true);
-        setDisplay("details");
-    }
-
-    function updateData() {
-        let updatedArr = [...arr];
-        updatedArr[indexVal] = obj;
-        setArr(updatedArr);
-        setFilArr(updatedArr);
-        setBool(false);
-        setObj({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: 0, experience: [{ company: "", role: "", years: "", stDate: "", enDate: "" }] });
-    }
-
+   
     return (
         <>
 
-            <div className="head">
-                <div><button className={display === 'details' ? 'hbtn hbtn_active' : 'hbtn'} onClick={() => setDisplay("details")}>Details</button></div>
-                <div><button className={display === 'experience' ? 'hbtn hbtn_active' : 'hbtn'} onClick={() => setDisplay("experience")}>Experience</button></div>
-            </div>
-
-
-            <div className="btndiv">
-                <button className="btn" type="button" onClick={!bool ? saveData : updateData}>{!bool ? 'Save' : 'Update'}</button>
-            </div>
+            <nav className="navbar">
+                <div className="toggle_btn_container">
+                    <button className={display === 'details' ? 'toggel_btn toggel_btn_active' : 'toggel_btn'} onClick={() => setDisplay("details")}>Details</button>
+                    <button className={display === 'experience' ? 'toggel_btn toggel_btn_active' : 'toggel_btn'} onClick={() => setDisplay("experience")}>Experience</button>
+                </div>
+                
+                <button className="save_btn" type="button" onClick={!bool ? saveData : updateData}>{!bool ? 'Save' : 'Update'}</button>
+            </nav>
 
             {display === "details" && (
                 <div className="container">
@@ -128,7 +127,7 @@ const NavBar = () => {
                         <input type="number" value={obj.age} onChange={(e) => setObj({ ...obj, age: e.target.value })} placeholder=" Age" required />
                         <input type="type" onFocus={(e) => (e.target.type = "date")} onBlur={(e) => (e.target.type = "text")} value={obj.doj} onChange={(e) => setObj({ ...obj, doj: e.target.value })} placeholder=" Date of Joining" />
                         <select value={obj.gender} onChange={(e) => setObj({ ...obj, gender: e.target.value })} required>
-                            <option value="">Select Gender</option>
+                            <option value="" disabled style={{ color: "black" }}>Select Gender</option>
                             <option style={{ color: "black" }} value="male">Male</option>
                             <option style={{ color: "black" }} value="female">Female</option>
                         </select>
@@ -139,8 +138,11 @@ const NavBar = () => {
 
             {display === "experience" && (
                 <div className="prevContainer">
-                    <span className="spanPre">Previous Company Details</span>
-                    <span><AddIcon onClick={addExperience} style={{ fontSize: "30px", cursor: "pointer", marginLeft: "3px", verticalAlign: "top" }} /></span>
+                    <div className="heading_bar">
+                        <span className="spanPre">Previous Company Details</span>
+                        <span><AddIcon onClick={addExperience} style={{ fontSize: "30px", cursor: "pointer", marginLeft: "3px", verticalAlign: "top" }} /></span>
+                    </div>
+                    
                     <div className="expDiv">
                         {obj.experience.map((exp, index) => {
                             const { company, role, years, stDate, enDate } = exp;
@@ -148,7 +150,7 @@ const NavBar = () => {
                                 <>
                                     { index === 0 ? null : <div className="line"></div>}
                                     <form key={index} className="experContainer">
-                                        <DeleteIcon onClick={() => deleteExperi(index)} style={{ marginLeft: "250px", fontSize: "25px", marginTop: "9px" }} />
+                                        <DeleteIcon onClick={() => deleteExperi(index)} style={{ marginLeft: "267px", fontSize: "25px"}} />
                                         <input type="text" value={company} onChange={(e) => experienceObj(index, "company", e.target.value)} placeholder="Company" />
                                         <input type="text" value={role} onChange={(e) => experienceObj(index, "role", e.target.value)} placeholder="Role" />
                                         <input type="number" value={years} onChange={(e) => experienceObj(index, "years", e.target.value)} placeholder="Years" />
@@ -163,8 +165,8 @@ const NavBar = () => {
                 </div>
             )}
 
-            <div className="TableBox">
-                <table className="table" style={{ borderCollapse: "collapse", width: "100%", textAlign: "left" }}>
+            <div className="tableBox">
+                <table className="table" >
                     <thead>
                         <tr>
                             <th>S.No</th>
@@ -203,8 +205,8 @@ const NavBar = () => {
                                     <td>{obj.doj}</td>
                                     <td>{obj.gender}</td>
                                     <td >{obj.totalExperience}</td>
-                                    <td style={{textAlign:"center"}}><button onClick={() => editData(i)}>Edit</button></td>
-                                    <td style={{textAlign:"center"}}><button onClick={() => deleteData(i)}>Delete</button></td>
+                                    <td><button onClick={() => editData(i)}>Edit</button></td>
+                                    <td><button onClick={() => deleteData(i)}>Delete</button></td>
                                 </tr>
                             )) : <tr><td colSpan={10} style={{textAlign:"center"}}>No Data</td></tr>}
                     </tbody>
