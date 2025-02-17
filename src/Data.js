0b9c251c58a77a417039a1e92d7dbedf66c2ee56
rajasekhar29112001 <krajasekhar2901@gmail.com>
2025-02-17 10:57:15 +0530

    final employee form

---
+++ src/Data.js
@@ -0,0 +1,175 @@
+[
+    {
+        "id": "100",
+        "name": "raja",
+        "email": "raja@gmail.com",
+        "age": "23",
+        "doj": "2025-02-19",
+        "gender": "male",
+        "totalExperience": 5,
+        "experience": [
+            {
+                "company": "lima",
+                "role": "sd",
+                "years": "3",
+                "stDate": "2025-02-02",
+                "enDate": "2025-02-05"
+            },
+            {
+                "company": "ibm",
+                "role": "se",
+                "years": "2",
+                "stDate": "2025-02-04",
+                "enDate": "2025-02-10"
+            }
+        ]
+    },
+    {
+        "id": "200",
+        "name": "venu",
+        "email": "venu@gmail.com",
+        "age": "21",
+        "doj": "2025-02-19",
+        "gender": "male",
+        "totalExperience": 7,
+        "experience": [
+            {
+                "company": "lima",
+                "role": "sa",
+                "years": "3",
+                "stDate": "2025-02-13",
+                "enDate": "2025-03-06"
+            },
+            {
+                "company": "ibm",
+                "role": "as",
+                "years": "4",
+                "stDate": "2025-02-12",
+                "enDate": "2025-02-19"
+            }
+        ]
+    },
+    {
+        "id": "300",
+        "name": "candy",
+        "email": "candy@gmail.com",
+        "age": "65",
+        "doj": "2025-02-04",
+        "gender": "female",
+        "totalExperience": 6,
+        "experience": [
+            {
+                "company": "lima",
+                "role": "sd",
+                "years": "6",
+                "stDate": "2025-01-30",
+                "enDate": "2025-02-18"
+            }
+        ]
+    },
+    {
+        "id": "400",
+        "name": "sandy",
+        "email": "sandy@gmail.com",
+        "age": "38",
+        "doj": "2025-01-28",
+        "gender": "female",
+        "totalExperience": 4,
+        "experience": [
+            {
+                "company": "lima",
+                "role": "sf",
+                "years": "4",
+                "stDate": "2025-02-05",
+                "enDate": "2025-02-26"
+            }
+        ]
+    },
+    {
+        "id": "500",
+        "name": "bob",
+        "email": "bob@gmail.com",
+        "age": "36",
+        "doj": "2025-02-09",
+        "gender": "male",
+        "totalExperience": 8,
+        "experience": [
+            {
+                "company": "lima",
+                "role": "sd",
+                "years": "8",
+                "stDate": "2025-02-13",
+                "enDate": "2025-02-25"
+            }
+        ]
+    },
+    {
+        "id": "600",
+        "name": "sekhar",
+        "email": "sekhar@gmail.com",
+        "age": "25",
+        "doj": "2025-02-09",
+        "gender": "male",
+        "totalExperience": 0,
+        "experience": [
+            {
+                "company": "",
+                "role": "",
+                "years": "",
+                "stDate": "",
+                "enDate": ""
+            }
+        ]
+    },
+    {
+        "id": "700",
+        "name": "sam",
+        "email": "sam@gmail",
+        "age": "39",
+        "doj": "2025-02-06",
+        "gender": "female",
+        "totalExperience": 1,
+        "experience": [
+            {
+                "company": "lima",
+                "role": "sd",
+                "years": "1",
+                "stDate": "2025-01-29",
+                "enDate": "2025-02-19"
+            }
+        ]
+    },
+    {
+        "id": "800",
+        "name": "virat",
+        "email": "virat@gmail.com",
+        "age": "46",
+        "doj": "2025-02-19",
+        "gender": "male",
+        "totalExperience": 26,
+        "experience": [
+            {
+                "company": "india",
+                "role": "bat",
+                "years": "14",
+                "stDate": "2025-02-05",
+                "enDate": "2025-02-12"
+            },
+            {
+                "company": "ranji",
+                "role": "keeper",
+                "years": "2",
+                "stDate": "2025-02-06",
+                "enDate": "2025-02-18"
+            },
+            {
+                "company": "ipl",
+                "role": "captain",
+                "years": "10",
+                "stDate": "2025-02-03",
+                "enDate": "2025-02-16"
+            }
+        ]
+    }
+]
+
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,18 +6,374 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const NavBar = () => {
     const [display, setDisplay] = useState("details");
-    const [arr, setArr] = useState([]);
+    const [arr, setArr] = useState([
+        {
+            "id": "100",
+            "name": "raja",
+            "email": "raja@gmail.com",
+            "age": "23",
+            "doj": "2025-02-19",
+            "gender": "male",
+            "totalExperience": 5,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "3",
+                    "stDate": "2025-02-02",
+                    "enDate": "2025-02-05"
+                },
+                {
+                    "company": "ibm",
+                    "role": "se",
+                    "years": "2",
+                    "stDate": "2025-02-04",
+                    "enDate": "2025-02-10"
+                }
+            ]
+        },
+        {
+            "id": "200",
+            "name": "venu",
+            "email": "venu@gmail.com",
+            "age": "21",
+            "doj": "2025-02-19",
+            "gender": "male",
+            "totalExperience": 7,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sa",
+                    "years": "3",
+                    "stDate": "2025-02-13",
+                    "enDate": "2025-03-06"
+                },
+                {
+                    "company": "ibm",
+                    "role": "as",
+                    "years": "4",
+                    "stDate": "2025-02-12",
+                    "enDate": "2025-02-19"
+                }
+            ]
+        },
+        {
+            "id": "300",
+            "name": "candy",
+            "email": "candy@gmail.com",
+            "age": "65",
+            "doj": "2025-02-04",
+            "gender": "female",
+            "totalExperience": 6,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "6",
+                    "stDate": "2025-01-30",
+                    "enDate": "2025-02-18"
+                }
+            ]
+        },
+        {
+            "id": "400",
+            "name": "sandy",
+            "email": "sandy@gmail.com",
+            "age": "38",
+            "doj": "2025-01-28",
+            "gender": "female",
+            "totalExperience": 4,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sf",
+                    "years": "4",
+                    "stDate": "2025-02-05",
+                    "enDate": "2025-02-26"
+                }
+            ]
+        },
+        {
+            "id": "500",
+            "name": "bob",
+            "email": "bob@gmail.com",
+            "age": "36",
+            "doj": "2025-02-09",
+            "gender": "male",
+            "totalExperience": 8,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "8",
+                    "stDate": "2025-02-13",
+                    "enDate": "2025-02-25"
+                }
+            ]
+        },
+        {
+            "id": "600",
+            "name": "sekhar",
+            "email": "sekhar@gmail.com",
+            "age": "25",
+            "doj": "2025-02-09",
+            "gender": "male",
+            "totalExperience": 0,
+            "experience": [
+                {
+                    "company": "",
+                    "role": "",
+                    "years": "",
+                    "stDate": "",
+                    "enDate": ""
+                }
+            ]
+        },
+        {
+            "id": "700",
+            "name": "sam",
+            "email": "sam@gmail",
+            "age": "39",
+            "doj": "2025-02-06",
+            "gender": "female",
+            "totalExperience": 1,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "1",
+                    "stDate": "2025-01-29",
+                    "enDate": "2025-02-19"
+                }
+            ]
+        },
+        {
+            "id": "800",
+            "name": "virat",
+            "email": "virat@gmail.com",
+            "age": "46",
+            "doj": "2025-02-19",
+            "gender": "male",
+            "totalExperience": 26,
+            "experience": [
+                {
+                    "company": "india",
+                    "role": "bat",
+                    "years": "14",
+                    "stDate": "2025-02-05",
+                    "enDate": "2025-02-12"
+                },
+                {
+                    "company": "ranji",
+                    "role": "keeper",
+                    "years": "2",
+                    "stDate": "2025-02-06",
+                    "enDate": "2025-02-18"
+                },
+                {
+                    "company": "ipl",
+                    "role": "captain",
+                    "years": "10",
+                    "stDate": "2025-02-03",
+                    "enDate": "2025-02-16"
+                }
+            ]
+        }
+    ]
+    
+    );
     const [obj, setObj] = useState({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: 0, experience: [{ company: "", role: "", years: "", stDate: "", enDate: "" }] });
 
     const [indexVal, setIndexVal] = useState(null);
     const [bool, setBool] = useState(false);
 
     const [searchObj, setSearchObj] = useState({ id: "", name: "", email: "", age: "", doj: "", gender: "", totalExperience: "" });
-    const [filArr, setFilArr] = useState([]);
+    const [filArr, setFilArr] = useState([
+        {
+            "id": "100",
+            "name": "raja",
+            "email": "raja@gmail.com",
+            "age": "23",
+            "doj": "2025-02-19",
+            "gender": "male",
+            "totalExperience": 5,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "3",
+                    "stDate": "2025-02-02",
+                    "enDate": "2025-02-05"
+                },
+                {
+                    "company": "ibm",
+                    "role": "se",
+                    "years": "2",
+                    "stDate": "2025-02-04",
+                    "enDate": "2025-02-10"
+                }
+            ]
+        },
+        {
+            "id": "200",
+            "name": "venu",
+            "email": "venu@gmail.com",
+            "age": "21",
+            "doj": "2025-02-19",
+            "gender": "male",
+            "totalExperience": 7,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sa",
+                    "years": "3",
+                    "stDate": "2025-02-13",
+                    "enDate": "2025-03-06"
+                },
+                {
+                    "company": "ibm",
+                    "role": "as",
+                    "years": "4",
+                    "stDate": "2025-02-12",
+                    "enDate": "2025-02-19"
+                }
+            ]
+        },
+        {
+            "id": "300",
+            "name": "candy",
+            "email": "candy@gmail.com",
+            "age": "65",
+            "doj": "2025-02-04",
+            "gender": "female",
+            "totalExperience": 6,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "6",
+                    "stDate": "2025-01-30",
+                    "enDate": "2025-02-18"
+                }
+            ]
+        },
+        {
+            "id": "400",
+            "name": "sandy",
+            "email": "sandy@gmail.com",
+            "age": "38",
+            "doj": "2025-01-28",
+            "gender": "female",
+            "totalExperience": 4,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sf",
+                    "years": "4",
+                    "stDate": "2025-02-05",
+                    "enDate": "2025-02-26"
+                }
+            ]
+        },
+        {
+            "id": "500",
+            "name": "bob",
+            "email": "bob@gmail.com",
+            "age": "36",
+            "doj": "2025-02-09",
+            "gender": "male",
+            "totalExperience": 8,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "8",
+                    "stDate": "2025-02-13",
+                    "enDate": "2025-02-25"
+                }
+            ]
+        },
+        {
+            "id": "600",
+            "name": "sekhar",
+            "email": "sekhar@gmail.com",
+            "age": "25",
+            "doj": "2025-02-09",
+            "gender": "male",
+            "totalExperience": 0,
+            "experience": [
+                {
+                    "company": "",
+                    "role": "",
+                    "years": "",
+                    "stDate": "",
+                    "enDate": ""
+                }
+            ]
+        },
+        {
+            "id": "700",
+            "name": "sam",
+            "email": "sam@gmail",
+            "age": "39",
+            "doj": "2025-02-06",
+            "gender": "female",
+            "totalExperience": 1,
+            "experience": [
+                {
+                    "company": "lima",
+                    "role": "sd",
+                    "years": "1",
+                    "stDate": "2025-01-29",
+                    "enDate": "2025-02-19"
+                }
+            ]
+        },
+        {
+            "id": "800",
+            "name": "virat",
+            "email": "virat@gmail.com",
+            "age": "46",
+            "doj": "2025-02-19",
+            "gender": "male",
+            "totalExperience": 26,
+            "experience": [
+                {
+                    "company": "india",
+                    "role": "bat",
+                    "years": "14",
+                    "stDate": "2025-02-05",
+                    "enDate": "2025-02-12"
+                },
+                {
+                    "company": "ranji",
+                    "role": "keeper",
+                    "years": "2",
+                    "stDate": "2025-02-06",
+                    "enDate": "2025-02-18"
+                },
+                {
+                    "company": "ipl",
+                    "role": "captain",
+                    "years": "10",
+                    "stDate": "2025-02-03",
+                    "enDate": "2025-02-16"
+                }
+            ]
+        }
+    ]
     
+    );
+
+    console.log("arr",arr)
     function addExperience() {
         setObj({ ...obj, experience: [...obj.experience, { company: "", role: "", years: "", stDate: "", enDate: "" }] });
     }
+
+    let namesArr = ['raja','sekhar','venu','gopal','naren','santhu'];
+    console.log("namesArr1 = ",namesArr)
+    namesArr.sort();
+    console.log("namesArr2 = ",namesArr)
 
     function saveData() {
         (!obj.id || !obj.name || !obj.email || !obj.age || !obj.doj || !obj.gender) ? alert("Please enter all fields") : filterDupData();
@@ -90,7 +446,6 @@ const NavBar = () => {
         setObj({ ...obj, experience: updateExper });
         sumOfYears({ ...obj, experience: updateExper });
     }
-    console.log("obj",obj)
 
     function sumOfYears(data) {
         let sum = data.experience.reduce((total, iVal) => total + Number(iVal.years), 0);
